a63459d35d413117de505f98fab2b6ecd7ef8c49
mr07s <106861707+mr07s@users.noreply.github.com>
2024-10-28 12:30:46 +0530

    Authentication updated

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { NextResponse } from "next/server";
 import GoogleProvider from "next-auth/providers/google";
+import { prismaClient } from "@/app/lib/db";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -8,6 +9,27 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  callbacks: {
+    async signIn(params) {
+      if (!params?.user?.email) {
+        return false;
+      }
+      try {
+        await prismaClient.user.create({
+          data: {
+            email: params?.user?.email,
+            provider: "Google",
+          },
+        });
+      } catch (e) {
+        NextResponse.json(
+          { message: "Error While Authentication" },
+          { status: 411 }
+        );
+      }
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
